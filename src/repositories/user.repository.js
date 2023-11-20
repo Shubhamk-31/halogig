ee81166d3ebbff7a6ac6ba7f08ef2ea6c78cccdb
Tanishq510 <tanishqjoshi93@gmail.com>
2023-11-20 11:23:56 +0530

    resolved password issue in user registration api

--- src/repositories/user.repository.js
+++ src/repositories/user.repository.js
@@ -62,7 +62,9 @@ export default {
   async userRegistration(req) {
     try {
       const { body } = req;
-      body.password = await utils.generateHashPassword(body.password);
+      if (body.password) {
+        body.password = await utils.generateHashPassword(body.password);
+      }
       await User.update(body, { where: { id: body.id } });
       const token = await jwt.createToken({ id: body.id });
       return token;
