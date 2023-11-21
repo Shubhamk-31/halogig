1a3bc522336971c77892297d9cbcb50b9bef1d78
shubhamk-31 <shubhamk_31@hotmail.com>
2023-11-21 23:21:35 +0530

    Add: routes for certificate and education

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -74,6 +74,27 @@ export default {
     }
   },
 
+  async userEducation(req, res, next) {
+    try {
+      const result = await userRepository.userEducation(req);
+      if (result) {
+        res.status(utility.httpStatus('OK')).json({
+          success: true,
+          data: result,
+          message: utility.getMessage(req, false, ''),
+        });
+      } else {
+        res.status(utility.httpStatus('BAD_REQUEST')).json({
+          success: false,
+          data: null,
+          message: utility.getMessage(req, false, 'FALSE_RESPONSE'),
+        });
+      }
+    } catch (error) {
+      next(error);
+    }
+  },
+
   async userDetails(req, res, next) {
     try {
       const result = await userRepository.userProjectDetail(req);
