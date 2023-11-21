5704ee49c2d77b9f94ed53f99ebf1fd4ca7266ed
shubhamk-31 <shubhamk_31@hotmail.com>
2023-11-21 23:16:37 +0530

    Add: education data

--- src/models/Education.model.js
+++ src/models/Education.model.js
@@ -1,15 +1,41 @@
-// module.exports = (sequelize, DataTypes) => {
-//     const Education = sequelize.define('Education', {
-//       name: {
-//         type: DataTypes.STRING,
-//       },
-//       countries_isd_code: {
-//         type: DataTypes.TEXT,
-//       },
-//     }, {
-//       underscored: true,
-//     });
-  
-//     return Education;
-//   };
-  
+module.exports = (sequelize, DataTypes) => {
+  const Education = sequelize.define('Education', {
+    bachelor: {
+      type: DataTypes.STRING,
+    },
+    userId: {
+      type: DataTypes.STRING,
+    },
+    bachelorUniversity: {
+      type: DataTypes.STRING,
+    },
+    bachelorFrom: {
+      type: DataTypes.STRING,
+    },
+    bachelorTo: {
+      type: DataTypes.STRING,
+    },
+    bachelorType: {
+      type: DataTypes.STRING,
+    },
+    master: {
+      type: DataTypes.STRING,
+    },
+    masterUniversity: {
+      type: DataTypes.STRING,
+    },
+    masterFrom: {
+      type: DataTypes.STRING,
+    },
+    masterTo: {
+      type: DataTypes.STRING,
+    },
+    masterType: {
+      type: DataTypes.STRING,
+    },
+  }, {
+    underscored: true,
+  });
+
+  return Education;
+};
