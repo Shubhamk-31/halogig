cec28b538a7709967d2f4c21dfdeb95a3266aa68
Tanishq510 <tanishqjoshi93@gmail.com>
2023-11-22 15:46:08 +0530

    added industry

---
+++ src/controllers/industry.controller.js
@@ -0,0 +1,56 @@
+import repositories from '../repositories';
+import utility from '../utils';
+
+const { industryRepository } = repositories;
+
+export default {
+  /**
+   * Create Industry
+   * @param {object} req
+   * @param {object} res
+   * @param {Function} next
+   */
+  async createIndustry(req, res, next) {
+    try {
+      const result = await industryRepository.createIndustry(req);
+      if (result) {
+        res.status(utility.httpStatus('OK')).json({
+          success: true,
+          data: result,
+          message: utility.getMessage(req, false, ''),
+        });
+      } else {
+        res.status(utility.httpStatus('BAD_REQUEST')).json({
+          success: false,
+          data: null,
+          message: utility.getMessage(req, false, 'Industry Already Exist'),
+        });
+      }
+    } catch (error) {
+      next(error);
+    }
+  },
+  /**
+   * Function to get list of industries
+   */
+  async getIndustry(req, res, next) {
+    try {
+      const result = await industryRepository.getIndustry();
+      if (result) {
+        res.status(utility.httpStatus('OK')).json({
+          success: true,
+          data: result,
+          message: utility.getMessage(req, false, ''),
+        });
+      } else {
+        res.status(utility.httpStatus('BAD_REQUEST')).json({
+          success: false,
+          data: null,
+          message: utility.getMessage(req, false, 'Industry Already Exist'),
+        });
+      }
+    } catch (error) {
+      next(error);
+    }
+  },
+};
---
+++ src/repositories/industry.repository.js
@@ -0,0 +1,29 @@
+import models from '../models';
+
+const { Industry } = models;
+export default {
+  /**
+   * Function to create industry
+   * @param {*} req
+   * @returns
+   */
+  async createIndustry(req) {
+    try {
+      const { body } = req;
+      return Industry.create(body);
+    } catch (error) {
+      throw Error(error);
+    }
+  },
+  /**
+   * Function to get list of industry
+   * @returns
+   */
+  async getIndustry() {
+    try {
+      return Industry.findAll();
+    } catch (error) {
+      throw Error(error);
+    }
+  },
+};
